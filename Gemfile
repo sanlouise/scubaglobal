969b7b302de3a194bab933218f2bb139a58d8bae
sandralouise <sandra.adamshallie@gmail.com>
2016-02-11 00:02:09 +0000

    Prepare for AWS3

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # gem 'therubyracer', platforms: :ruby
 # For image uploads in production
 gem 'carrierwave'
+gem 'fog-aws'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     'xxx',                        # required
+    aws_secret_access_key: 'yyy',                        # required
+    region:                'eu-central-1',                  # optional, defaults to 'us-east-1'
+
+  }
+  config.fog_directory  = 'name_of_directory'                          # required
+
+end
