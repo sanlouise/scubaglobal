c2f6d2fedf6717a7bc77517bf15a914ddc0461f1
sandralouise <sandra.adamshallie@gmail.com>
2016-02-11 13:05:50 +0000

    Set up AWS3 for production

--- Gemfile
+++ Gemfile
@@ -14,7 +14,9 @@ gem 'coffee-rails', '~> 4.1.0'
 # gem 'therubyracer', platforms: :ruby
 # For image uploads in production
 gem 'carrierwave'
-gem 'fog-aws'
+gem 'fog'
+# Protect passwords
+gem 'figaro'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
+
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'xxx',                        # required
-    aws_secret_access_key: 'yyy',                        # required
+    aws_access_key_id:     ENV["aws_access_key_id"],                        # required
+    aws_secret_access_key: ENV["aws_secret_access_key"],                        # required
     region:                'eu-central-1',                  # optional, defaults to 'us-east-1'
 
   }
-  config.fog_directory  = 'name_of_directory'                          # required
+  config.fog_directory  = ENV["fog_directory"]                         # required
 
 end
