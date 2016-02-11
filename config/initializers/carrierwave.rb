d4965f333fd73d6d2391b89227d28b2bf65cda33
sandralouise <sandra.adamshallie@gmail.com>
2016-02-11 13:19:52 +0000

    Delete region from carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,6 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["aws_access_key_id"],                        # required
     aws_secret_access_key: ENV["aws_secret_access_key"],                        # required
-    region:                'eu-central-1',                  # optional, defaults to 'us-east-1'
 
   }
   config.fog_directory  = ENV["fog_directory"]                         # required
