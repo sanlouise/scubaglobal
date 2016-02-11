46705413e4a35b36077e6f04536a4c36ae6b491a
sandralouise <sandra.adamshallie@gmail.com>
2016-02-11 14:45:08 +0000

    Update carrierwave initializer with new environment variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
-CarrierWave.configure do |config|
-
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["aws_access_key_id"],                        # required
-    aws_secret_access_key: ENV["aws_secret_access_key"],                        # required
-    region: ENV["region"]
-
-  }
-  config.fog_directory  = ENV["fog_directory"]                         # required
-
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region => 'eu-central-1'
+    }
+    
+    config.fog_directory = ENV['S3_BUCKET']
+  end
+  
 end
-
