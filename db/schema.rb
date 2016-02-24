1b4ae5fc9e4e2d77558deb66d503f6b028fef4ec
sandralouise <sandra.adamshallie@gmail.com>
2016-02-24 13:21:55 +0000

    Add user id to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212084618) do
+ActiveRecord::Schema.define(version: 20160224131812) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "schools", force: :cascade do |t|
